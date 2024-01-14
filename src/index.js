e752769e9297b4f52e5ecba76bd13c7f60fa7d41
paul <paul.thiongo@student.moringaschool.com>
2024-01-14 19:24:08 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// index.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const tasksList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const taskDescription = taskInput.value;
+    if (taskDescription) {
+      addTask(taskDescription);
+      form.reset();
+    }
+  });
+
+  function addTask(description) {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = description;
+    tasksList.appendChild(taskItem);
+  }
 });
